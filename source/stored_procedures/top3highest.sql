df8874243d2b9936bfcd0fb97190d02966707eb4
A  S PRAJNA <60390104+ASPRAJNA@users.noreply.github.com>
2023-08-01 14:48:12 +0530

    Delete  top3highest.sql

--- source/stored_procedures/top3highest.sql
+++
@@ -1,6 +0,0 @@
-SELECT TOP 1 [NumberOfConnections]
-FROM 
-    (SELECT TOP 3 [NumberOfConnections]
-     FROM [mssql_to_pgsql_pgloader].[dbo].[activeuserconnections] 
-     ORDER BY[NumberOfConnections] DESC ) AS Comp 
-ORDER BY [NumberOfConnections];
